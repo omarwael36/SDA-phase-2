4e446c8783a55ba8234092fe04c6a9c711e80d63
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2022-12-29 18:03:23 +0200

    Project created

---
+++ Fawry-System/src/main/java/com/SDA/phase2/FawrySystem/FawrySystemApplication.java
@@ -0,0 +1,14 @@
+package com.SDA.phase2.FawrySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawrySystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FawrySystemApplication.class, args);
+	}
+
+}
---
+++ Fawry-System/src/test/java/com/SDA/phase2/FawrySystem/FawrySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SDA.phase2.FawrySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FawrySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
