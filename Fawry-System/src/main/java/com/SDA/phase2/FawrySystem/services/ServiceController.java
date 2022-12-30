d16114146dc0981dc566e5a7a0b1e83b90357878
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2022-12-30 21:51:50 +0200

    improvments

--- Fawry-System/src/main/java/com/SDA/phase2/FawrySystem/services/ServiceController.java
+++ Fawry-System/src/main/java/com/SDA/phase2/FawrySystem/services/ServiceController.java
@@ -2,6 +2,8 @@ package com.SDA.phase2.FawrySystem.services;
 
 import java.util.ArrayList;
 
+
+
 public class ServiceController {
     public ArrayList<DiscountInfo> Discounts = new ArrayList<DiscountInfo>();
     public ArrayList<ServiceInfo> services = new ArrayList<ServiceInfo>();
@@ -18,17 +20,18 @@ public class ServiceController {
         }
         return flag;
     }
-    public double SelectSP(int spid,String serviceName){
+    public Object SelectSP(int spid, String serviceName){
         for(int i = 0; i<services.size();i++){
             if(services.get(i).getName()==serviceName){
                 for(int j = 0 ; j<services.get(i).SP.size() ; j++){
                     if(services.get(i).SP.get(j).getSPID()==spid){
-                        return services.get(i).SP.get(j).getTax();
+                        return services.get(i).SP.get(j);
                     }
                 }
             }
         }
-        return 0;
+       ServiceProvider sp = new ServiceProvider(" ",0);
+        return sp;
     }
     public void UpdatePrice(double amount,String serviceName){
         for(int i = 0; i<services.size();i++){
