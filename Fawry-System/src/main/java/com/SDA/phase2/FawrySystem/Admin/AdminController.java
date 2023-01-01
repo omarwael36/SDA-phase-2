bfe1da9fde04e0235d2bf9e53e5869b5670a695f
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2023-01-01 23:59:01 +0200

    final

--- Fawry-System/src/main/java/com/SDA/phase2/FawrySystem/Admin/AdminController.java
+++ Fawry-System/src/main/java/com/SDA/phase2/FawrySystem/Admin/AdminController.java
@@ -1,26 +1,41 @@
 package com.SDA.phase2.FawrySystem.Admin;
 
 import com.SDA.phase2.FawrySystem.Refund.RefundController;
+import com.SDA.phase2.FawrySystem.Refund.Transactions;
+import com.SDA.phase2.FawrySystem.User.UserController;
+import com.SDA.phase2.FawrySystem.User.Userinfo;
 import com.SDA.phase2.FawrySystem.services.DiscountInfo;
 import com.SDA.phase2.FawrySystem.services.ServiceController;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 
+@Service
 public class AdminController {
     ServiceController sc = new ServiceController();
+    UserController uc = new UserController();
     RefundController refund = new RefundController();
     public void AddnewSP ()
     {
 
     }
+    public Userinfo SearchUser(String username){
+        for(int i = 0; i<uc.Users.size();i++){
+            if(uc.Users.get(i).getusername().equals(username)){
+                return uc.Users.get(i);
+            }
+        }
+        Userinfo ui= new Userinfo();
+        return ui;
+    }
     public void AddDiscount (double rate, String name, String type, int ID,String dison)
     {
         DiscountInfo di = new DiscountInfo(rate,name,type,ID,dison);
         sc.Discounts.add(di);
-
     }
-    public void ShowUserTransactions ()
+    public ArrayList<Transactions> ShowUserTransactions (Userinfo user)
     {
-        //Show
+        return user.getUTransactions();
     }
     public void ShowRefund (int id)
     {
